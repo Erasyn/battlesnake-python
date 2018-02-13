b9d14a13a733b16b324eb00437976df4684bc0e7
Ethan Wipond <ewipond@gmail.com>
2018-02-12 16:38:29 -0800

    'Working' dumb snake

--- app/main.py
+++ app/main.py
@@ -2,6 +2,33 @@ import bottle
 import os
 import random
 
+class Point:
+    '''Simple class for 2d points'''
+
+    def __init__(self, x, y):
+        '''Defines x and y variables'''
+        self.x = x
+        self.y = y 
+
+    def __eq__(self, other):
+        '''Test equality'''
+        return self.x == other.x and self.y == other.y
+
+    def left(self):
+        '''Get the point to the left'''
+        return Point(self.x-1, self.y)
+
+    def right(self):
+        '''Get the point to the right'''
+        return Point(self.x+1, self.y)
+
+    def up(self):
+        '''Get the point above'''
+        return Point(self.x, self.y-1)
+
+    def down(self):
+        '''Get the point below'''
+        return Point(self.x, self.y+1)
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -20,8 +47,6 @@ def start():
         bottle.request.urlparts.netloc
     )
 
-    # TODO: Do things with data
-
     return {
         'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
@@ -34,12 +59,52 @@ def start():
 def move():
     data = bottle.request.json
 
-    # TODO: Do things with data
-    directions = ['up', 'down', 'left', 'right']
+    dim = Point(data['width'], data['height'])
+    
+    head = Point(data['you']['body']['data'][0]['x'], 
+                 data['you']['body']['data'][0]['y'])
+
+    body_data = data['you']['body']['data'][1:]
+    body = []
+    for b in body_data:
+        body.append(Point(b['x'], b['y']))
+
+    possible_directions = ['up', 'down', 'left', 'right']
+
+    # Don't move into your body
+    if (head.right() in body and 'right' in possible_directions):
+        possible_directions.remove('right')
+    if (head.left() in body and 'left' in possible_directions):
+        possible_directions.remove('left')
+    if (head.up() in body and 'up' in possible_directions):
+        possible_directions.remove('up')
+    if (head.down() in body and 'down' in possible_directions):
+        possible_directions.remove('down')
+
+    # Don't move into the wall
+    if (head.left().x == 0 and 'left' in possible_directions):
+        possible_directions.remove('left')
+    if (head.right().x == dim.x and 'right' in possible_directions):
+        possible_directions.remove('right')
+    if (head.down().y == dim.y and 'down' in possible_directions):
+        possible_directions.remove('down')
+    if (head.up().y == 0 and 'up' in possible_directions):
+        possible_directions.remove('up')
+
+    # Move towards the first food
+    food = Point(data['food']['data'][0]['x'],
+                 data['food']['data'][0]['y'])
+    if (head.x > food.x and 'right' in possible_directions):
+        possible_directions.remove('right')
+    if (head.x < food.x and 'left' in possible_directions):
+        possible_directions.remove('left')
+    if (head.y < food.y and 'up' in possible_directions):
+        possible_directions.remove('up')
+    if (head.y > food.y and 'down' in possible_directions):
+        possible_directions.remove('down')
 
     return {
-        #'move': random.choice(directions),
-        'move': 'up',
+        'move': random.choice(possible_directions),
         'taunt': 'battlesnake-python!'
     }
 
